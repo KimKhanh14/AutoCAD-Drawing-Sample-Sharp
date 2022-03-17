a5a35e9e36eda1c2117314f20d7be9fb2563ff42
KimKhanh14 <jennykhanh14@gmail.com>
2022-03-17 14:51:29 +0700

    Print message

---
+++ Source/PluginAutoCAD/PluginAutoCAD/Init.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using AcAp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+
+namespace PluginAutoCAD
+{
+    public class Init : IExtensionApplication
+    {
+        [CommandMethod("MyFirstCommand")]
+        public void cmdFirstCommand()
+        {
+            var doc = AcAp.DocumentManager.MdiActiveDocument;
+            //var db = doc.Database;
+            var ed = doc.Editor;
+
+            ed.WriteMessage("\nHello Khánh");
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public void Terminate()
+        {
+
+        }
+    }
+}
